68774676a072da34d23aaae28025e44439efb2cc
Hammad Hassan <hammadhassansheikh@gmail.com>
2017-08-20 13:07:08 +0500

    add vector icons

---
+++ index.android.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {  AppRegistry, StyleSheet, Text, View, } from 'react-native';
+import Home from "./src/Home";
+
+export default class PatientsTracker1 extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Welcome to Home Page
+        </Text>
+          <Home />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10,
+  // },
+  // instructions: {
+  //   textAlign: 'center',
+  //   color: '#333333',
+  //   marginBottom: 5,
+  // },
+  map: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('PatientsTracker1', () => PatientsTracker1);
